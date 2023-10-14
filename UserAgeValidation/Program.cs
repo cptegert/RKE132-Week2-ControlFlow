918a87fba797df5040d042c7e738cc531972994c
Egert Tappo <egert.tappo@tktk.ee>
2023-10-15 01:21:10 +0300

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Egert!");
+
+
+//mis sugu?? (m/f)
+//perekonnanimi? 
+//Tere..
+
+Console.WriteLine("Please select your gender(m/f):");
+
+char userGender = char.Parse(Console.ReadLine());
+Console.WriteLine("Please enter your name:");
+string userLastName = Console.ReadLine();
+//Console.WriteLine("Welcome "+ (userGender)+ "!");
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName} !");
+}
+else if (userGender == 'f')
+{
+    Console.WriteLine($"Welcome, Ms. {userLastName}!");
+}
+
+
+else
+{
+    Console.WriteLine("Welcome, " + (userLastName)+ "!");
+}
+
---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,43 @@
+﻿//vanus??
+
+//Console.WriteLine("Enter your age:");
+//int userAge = Int32.Parse(Console.ReadLine());
+
+
+//if (userAge > 12)
+//if (userAge>= 13)
+//{
+ //   Console.WriteLine("Welcome to Instagram!");
+//}
+//else
+//{
+//    Console.WriteLine("LIIGA noor!!");
+//}
+
+
+
+
+
+Console.WriteLine("Enter your age:");
+
+string userAge2 = Console.ReadLine();
+
+Int32 userAgeNumber = 0;
+
+bool userAgeNum = Int32.TryParse(userAge2, out userAgeNumber);
+
+if (userAgeNum == true)
+    if (userAgeNumber >= 13)
+    {
+        Console.WriteLine("Welcome to Instagram!");
+    }
+    else
+    {
+        Console.WriteLine("You are too young to use Instagram!");
+    }
+else
+{
+    Console.WriteLine("Could not read your entered age!");
+}
+
+
